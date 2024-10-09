5725d9a5c1dd76ad9b9203d4131c8e8d96509e9f
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-10-09 16:53:26 +0200

    add loadNFT function to item component

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,25 +1,50 @@
-import React from "react";
-import logo from "../../assets/logo.png";
+import React, { useEffect } from "react"
+import logo from "../../assets/logo.png"
+import { Actor, HttpAgent } from "@dfinity/agent"
+import {idlFactory} from "../../../declarations/nft"
+import { canisterId } from "../../../declarations/nft/index"
+import {Principal} from "@dfinity/principal"
 
-function Item() {
+function Item(props) {
+  //convert props to principal type
+  const id = Principal.fromText(props.id)
+
+  const localHost = "http://localhost:8080/"
+  const agent = new HttpAgent({host: localHost})
+
+  async function loadNFT() {
+    const NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id,
+    })
+  }
+
+  //call loadNFT function once when the page loads
+  useEffect(()=> {
+    loadNFT()
+  }, [])
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
+        {/* image */}
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
           src={logo}
         />
         <div className="disCardContent-root">
+        {/* name */}
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
             CryptoDunks #312<span className="purple-text"></span>
           </h2>
+        {/* owner */}
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
             Owner: sdfsdf-erwerv-sdf
           </p>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Item;
+export default Item
