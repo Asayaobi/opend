9fa85d878466a9f69979b10eaaa43dbf74c65532
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-10-10 17:44:56 +0200

    Update Item.jsx

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,25 +1,72 @@
-import React from "react";
-import logo from "../../assets/logo.png";
+import React, { useEffect, useState } from "react"
+import logo from "../../assets/logo.png"
+import { Actor, HttpAgent } from "@dfinity/agent"
+import {idlFactory} from "../../../declarations/nft"
+import { canisterId, createActor } from "../../../declarations/nft/index"
+import {Principal} from "@dfinity/principal"
 
-function Item() {
+function Item(props) {
+
+  const[name, setName]= useState()
+  const[owner, setOwner] = useState()
+  const[image, setImage] = useState()
+
+  //convert props to principal type
+  const id = Principal.fromText(props.id)
+
+  const localHost = "http://localhost:8080/"
+  const agent = new HttpAgent({host: localHost})
+
+  async function loadNFT() {
+    const NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id,
+    })
+    //call functions from nft.mo
+    const name = await NFTActor.getName()
+    setName(name)
+
+    const ownerPrincipal = await NFTActor.getOwner()
+    //convert Principal format to text
+    const ownerText= ownerPrincipal.toText()
+    setOwner(ownerText)
+
+    const imageData = await NFTActor.getAsset()
+    //convert the [NAT8] data into Uint8Array
+    const imageContent = new Uint8Array(imageData)
+    //turn it into the url
+    const image = URL.createObjectURL(
+      new Blob([imageContent.buffer], {type: "image/png"})
+    )
+    setImage(image)
+  }
+
+  //call loadNFT function once when the page loads
+  useEffect(()=> {
+    loadNFT()
+  }, [])
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
+        {/* image */}
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={image}
         />
         <div className="disCardContent-root">
+        {/* name */}
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {name}<span className="purple-text"></span>
           </h2>
+        {/* owner */}
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
+            Owner: {owner}
           </p>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Item;
+export default Item
