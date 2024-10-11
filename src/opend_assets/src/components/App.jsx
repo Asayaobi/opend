ec696d072df834981a85a11dfd828cb0d7393892
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-10-11 13:37:39 +0200

    Update App.jsx

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import "bootstrap/dist/css/bootstrap.min.css"
-import homeImage from "../../assets/home-img.png"
 import Item from "./Item"
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <div className="App">
       <Header />
       {/* <Item id={NFTID}/> */}
-      <img className="bottom-space" src={homeImage} />
       <Footer />
     </div>
   )
